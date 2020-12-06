521e7bcc3b9de342c8178e5512206fefdba5c522
Maxim Alexandrov <89265511699@mail.ru>
2020-12-06 19:27:45 +0300

    release new classes.1

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,63 +1,53 @@
-export const profileName = document.querySelector('.profile__name');                           // имя профиля 
-export const profileAbout = document.querySelector('.profile__about');                         // информация о себе 
-export const elementsList = document.querySelector('.elements__list');                         // контейнер карточек
-
-export const profilePopup = document.querySelector('.popup_profile');                          // попап профиля 
-export const profilePopupEditButton = document.querySelector('.profile__edit-btn');            // кнопка редактировать профиль 
-export const profilePopupCloseButton = profilePopup.querySelector('.popup__close-btn');        // кнопка закрыть форму радактора профиля 
-export const profileFormElement = profilePopup.querySelector('.popup__form');                  // форма профиля
-export const profileNameInput = profilePopup.querySelector('.popup__form-item_el_name');       // поле формы "имя" 
-export const profileJobInput = profilePopup.querySelector('.popup__form-item_el_about');       // поле формы "о себе" 
+const profilePopup = document.querySelector('.popup_profile');                          // попап профиля 
+const profilePopupEditButton = document.querySelector('.profile__edit-btn');            // кнопка редактировать профиль 
+const profileFormElement = profilePopup.querySelector('.popup__form');                  // форма профиля
+const profileNameInput = profilePopup.querySelector('.popup__form-item_el_name');       // поле формы "имя" 
+const profileJobInput = profilePopup.querySelector('.popup__form-item_el_about');       // поле формы "о себе" 
 
 
-export const cardPopup = document.querySelector('.popup_card');                                // попап карточки 
-export const cardPopupAddButton = document.querySelector('.profile__add-btn');                 // кнопка добавить карточку 
-export const cardPopupCloseButton =  cardPopup.querySelector('.popup__close-btn');             // кнопка закрыть форму добавления карточки 
-export const cardFormElement = cardPopup.querySelector('.popup__form');                        // форма карточки
-export const cardPlaceInput = cardPopup.querySelector('.popup__form-item_el_name');            // поле формы "название места" 
-export const cardLinkInput = cardPopup.querySelector('.popup__form-item_el_picture_link');     // поле формы "ссылка на изображение" 
+const cardPopup = document.querySelector('.popup_card');                                // попап карточки 
+const cardPopupAddButton = document.querySelector('.profile__add-btn');                 // кнопка добавить карточку 
+const cardFormElement = cardPopup.querySelector('.popup__form');                        // форма карточки
+
 
 export const imagePopup = document.querySelector('.popup_picture');                            // попап полного изображения
 export const imagePopupFullImage = imagePopup.querySelector('.popup__full-image');             // полное изображение
 export const imagePopupCloseButton =  imagePopup.querySelector('.popup__close-btn');           // кнопка закрыть полное изображение
 export const imagePopupCaption = imagePopup.querySelector('.popup__caption');                  // подпись полного изображения
 
-export const elementsTempateSelector = ('.elements__template');
 
-
-
-export const initialCards = [                                                                   //массив начальных карточек
-    {
-        name: 'Архыз',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-    },
-    {
-        name: 'Челябинская область',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-    },
-    {
-        name: 'Иваново',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-    },
-    {
-        name: 'Камчатка',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-    },
-    {
-        name: 'Холмогорский район',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-    },
-    {
-        name: 'Байкал',
-        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-    }
+const initialCards = [                                                                   //массив начальных карточек
+  {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
 ];
 
-export const validationSettings = {
-    inputElement: '.popup__form-item',
-    submitButtonSelector: '.popup__save-btn',
-    inactiveButtonClass: 'popup__save-btn_disabled',
-    inputErrorClass: 'popup__form-item_type_error',
-    errorClass: 'popup__error_visible',
-    lableSelector: '.popup__input-container'  
+const validationSettings = {
+  inputElement: '.popup__form-item',
+  submitButtonSelector: '.popup__save-btn',
+  inactiveButtonClass: 'popup__save-btn_disabled',
+  inputErrorClass: 'popup__form-item_type_error',
+  errorClass: 'popup__error_visible',
+  lableSelector: '.popup__input-container'  
 };
--- scripts/index.js
+++ scripts/index.js
@@ -5,61 +5,6 @@ import PopupWIthForm from '../scripts/PopupWIthForm.js';
 import PopupWithImage from '../scripts/PopupWithImage.js';
 import UserInfo from '../scripts/UserInfo.js';
 
-
-const profilePopup = document.querySelector('.popup_profile');                          // попап профиля 
-const profilePopupEditButton = document.querySelector('.profile__edit-btn');            // кнопка редактировать профиль 
-const profileFormElement = profilePopup.querySelector('.popup__form');                  // форма профиля
-const profileNameInput = profilePopup.querySelector('.popup__form-item_el_name');       // поле формы "имя" 
-const profileJobInput = profilePopup.querySelector('.popup__form-item_el_about');       // поле формы "о себе" 
-
-
-const cardPopup = document.querySelector('.popup_card');                                // попап карточки 
-const cardPopupAddButton = document.querySelector('.profile__add-btn');                 // кнопка добавить карточку 
-const cardFormElement = cardPopup.querySelector('.popup__form');                        // форма карточки
-
-
-export const imagePopup = document.querySelector('.popup_picture');                            // попап полного изображения
-export const imagePopupFullImage = imagePopup.querySelector('.popup__full-image');             // полное изображение
-export const imagePopupCloseButton =  imagePopup.querySelector('.popup__close-btn');           // кнопка закрыть полное изображение
-export const imagePopupCaption = imagePopup.querySelector('.popup__caption');                  // подпись полного изображения
-
-
-const initialCards = [                                                                   //массив начальных карточек
-  {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-
-const validationSettings = {
-  inputElement: '.popup__form-item',
-  submitButtonSelector: '.popup__save-btn',
-  inactiveButtonClass: 'popup__save-btn_disabled',
-  inputErrorClass: 'popup__form-item_type_error',
-  errorClass: 'popup__error_visible',
-  lableSelector: '.popup__input-container'  
-};
-
 const validCard = new FormValidator(validationSettings, cardFormElement);
 
 validCard.enableValidation();
